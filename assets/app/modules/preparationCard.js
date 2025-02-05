17c3445daf12042fe1d0e6d9c40fa7adf152a8ee
Осипович Дмитрий <d.osipovich@itgrade.ru>
2025-02-05 09:16:31 +0300

    fix: fixed a bugs after retest

--- assets/app/modules/preparationCard.js
+++ assets/app/modules/preparationCard.js
@@ -4,36 +4,31 @@ class PreparationCard {
 		this.cardList = document.querySelectorAll(cardListSelector)
 		this.cardMain = document.querySelector(cardMainSelector)
 
-		// Устанавливаем начальное изображение для главного блока
 		this.updateMainImage(this.cardList[0].querySelector('img').src)
 
-		// Устанавливаем начальные стили для первой миниатюры
 		this.highlightActiveThumbnail(this.cardList[0])
 
-		// Добавляем обработчики событий на миниатюры
 		this.cardList.forEach(item => {
 			item.addEventListener('click', () => {
-				const imgSrc = item.querySelector('img').src // Получаем ссылку на изображение миниатюры
-				this.updateMainImage(imgSrc) // Обновляем основное изображение
-				this.highlightActiveThumbnail(item) // Подсвечиваем активную миниатюру
+				const imgSrc = item.querySelector('img').src
+				this.updateMainImage(imgSrc)
+				this.highlightActiveThumbnail(item)
 			})
 		})
 	}
 
 	updateMainImage(imgSrc) {
-		this.cardMain.querySelector('img').src = imgSrc // Обновляем src основного изображения
+		this.cardMain.querySelector('img').src = imgSrc
 	}
 
 	highlightActiveThumbnail(activeItem) {
-		// Удаляем стили с всех миниатюр
 		this.cardList.forEach(item => {
-			item.style.backgroundColor = '' // Убираем фон
-			item.style.borderLeft = '' // Убираем левый бордер
+			item.style.backgroundColor = ''
+			item.style.borderLeft = ''
 		})
 
-		// Устанавливаем стили на активную миниатюру
-		activeItem.style.backgroundColor = '#ffffff' // Устанавливаем белый фон
-		activeItem.style.borderLeft = '2px solid #332e59' // Устанавливаем левый бордер
+		activeItem.style.backgroundColor = '#ffffff'
+		activeItem.style.borderLeft = '2px solid #332e59'
 	}
 }
 
