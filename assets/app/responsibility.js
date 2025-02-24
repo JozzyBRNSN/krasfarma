2d15761231c694124a8408229017d5043f5ffc86
Осипович Дмитрий <d.osipovich@itgrade.ru>
2025-02-24 14:36:52 +0300

    add change

---
+++ assets/app/responsibility.js
@@ -0,0 +1,67 @@
+import PopupArticle from './modules/popupArticle.js'
+import PopupHeader from './modules/popupHeader.js'
+import ContactUpdater from './modules/footerAddress.js'
+import StickyHeader from './modules/headerSticky.js'
+import ModalRegistration from './modules/modalRegistration.js'
+import ModalAuthorization from './modules/modalAuthorization.js'
+import ModalRecovery from './modules/modalRecovery.js'
+import modalMessage from './modules/modalMessage.js'
+
+document.addEventListener('DOMContentLoaded', () => {
+	const stickyHeader = new StickyHeader({
+		stickyPointSelector: '.sticky',
+		headerSelector: '.header',
+	})
+
+	const modalRegistration = new ModalRegistration({
+		modalWindowSelector: '.modal-registration',
+		modalOpenButtonSelector: '.header-registration__btn',
+		modalCloseButtonSelector: '.modal-close__btn',
+		modalWrapperSelector: '.modal-overlay',
+	})
+
+	const modalAuthorization = new ModalAuthorization({
+		modalWindowSelector: '.modal-authorization',
+		modalOpenButtonSelector: '.header-entrance__btn',
+		modalCloseButtonSelector: ['.recovery-password', '.modal-close__btn'],
+		modalWrapperSelector: '.modal-overlay',
+	})
+
+	const modalRecovery = new ModalRecovery({
+		modalWindowSelector: '.modal-recovery',
+		modalOpenButtonSelector: '.recovery-password',
+		modalCloseButtonSelector: '.modal-close__btn',
+		modalWrapperSelector: '.modal-overlay',
+		nextModalSelector: '.modal-message',
+		prevModalSelector: '.modal-authorization',
+		prevModalButtonSelector: '.modal-back__btn',
+	})
+
+	const modalMessages = new modalMessage({
+		modalWindowSelector: '.modal-message',
+		modalCloseButtonSelector: ['.modal-close__btn', '.modal-message__btn'],
+		modalWrapperSelector: '.modal-overlay',
+		formId: '#recovery-form',
+	})
+
+	const popupMission = new PopupArticle('.responsibility-mission__popup', '.mission-btn')
+
+	const popupHeader = new PopupHeader({
+		menuButtonId: 'header-nav__menu',
+		popupId: 'popup-menu',
+		buttonId: 'popup-btn',
+		burgerIds: [
+			'popup-burger--top',
+			'popup-burger--middle',
+			'popup-burger--bottom',
+		],
+		shadowMenu: '.popup-shadow',
+		popupLinkSelectors: '.popup-link',
+	})
+
+	const contactUpdater = new ContactUpdater({
+		cityElementsSelector: '.places-item',
+		addressElementsSelector: '.footer-address__link ',
+		phoneLinksSelector: '.footer-phone__link',
+	})
+})
